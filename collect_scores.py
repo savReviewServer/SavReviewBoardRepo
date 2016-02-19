c57340b8d56a91c5bfc8529506e6967025037989
Anna Obraztsova <alcedooo@gmail.com>
2016-02-19 18:27:20 +0300

    Добавила сортировку скоров, немного изменила порядок приписывания скоров генам

--- collect_scores.py
+++ collect_scores.py
@@ -40,38 +40,53 @@ print 'operons', len(operons)
 
 # создаём словарь геном:список_координат всех генов из оперонов, попавших в operons
 # такие списки можно будет отсортировать, чтобы искать скоры
-a = {x:[] for x in taxids}
+# в а - все гены с достаточными межгенниками
+# в b - все гены с нужными mog
+a = {x: [] for x in taxids}
+b = {x: {} for x in taxids}
 q = open('info/allBacteriaGenesInfo2.csv')
 qr = csv.DictReader(q, delimiter='\t')
+locus = '\xef\xbb\xbflocusId'  # костылик от кодировки
 for line in qr:
     if line['tuId'] in operons:
-        if line['\xef\xbb\xbflocusId'] in coords:
-            c = coords[line['\xef\xbb\xbflocusId']]+[line['\xef\xbb\xbflocusId'], line['mogId'], line['tuId']]
+        if line[locus] in coords:
+            c = coords[line[locus]]+[line[locus], line['mogId'], line['tuId']]
             a[line['taxonomyId']].append(c)
+    if line['taxonomyId'] in taxids and line['mogId'] in mog_names:
+        b[line['taxonomyId']][line['mogId']] = line[locus]
 q.close()
-print 'a',len(a)
+print 'a', len(a)
 
 # сортируем по первой координате
 for tax in a:
     a[tax].sort(key=lambda x: x[0])
 
+def sort_scores(f):
+    direction = {'+': 'false', '-': 'true'}
+    s1 = csv.DictReader(f, fieldnames=['chr', 'start', 'end', 'energy', 'score', 'strand'], delimiter='\t')
+    s2 = []
+    for i in s1:
+        newi = {'start': int(i['start']), 'end': int(i['end']), 'score': float(i['score']), 'strand': direction[i['strand']]}
+        s2.append(newi)
+    s2.sort(key=lambda x: (x['start'], x['end']))
+    return s2
+
 # извлекаем скоры по координатам
-direction = {'+':'false', '-':'true'}
 unavailable = []
 for tax in a:
     fname = '/mnt/mapr/user/chezoya/genomeScores/'+tax
     # fname = 'rgenes/'+tax
     if os.path.isfile(fname):
         s = open(fname)
-        sr = csv.DictReader(s, fieldnames=['chr','start','end','energy','score','strand'], delimiter='\t')
+        ss = sort_scores(s)
         n = 0
         scores = []
-        for line in sr:
+        for i in ss:
             if n < len(a[tax]):
                 gene_start, gene_end, gene_strand = a[tax][n][0], a[tax][n][1], a[tax][n][2]
-                if direction[line['strand']] == gene_strand and gene_start <= int(line['start'])<= int(line['end']) <= gene_end:
-                    scores.append(float(line['score']))
-                if int(line['end']) > gene_end:
+                if i['strand'] == gene_strand and gene_start <= i['start'] <= i['end'] <= gene_end:
+                    scores.append(i['score'])
+                if i['start'] > gene_end:
                     a[tax][n].append(min(scores+[0]))
                     scores = []
                     n += 1
@@ -99,13 +114,13 @@ for tax in a:
     o = open('rgenes/interScores/'+tax, 'a')
     o.write('locusID\tMOG\ttuID\tupstLen\tscore\n')
     for gene in a[tax]:
-        if len(gene)!=8:
+        if len(gene) != 8:
             print 'tax', tax
             print 'gene', gene
             print a[tax]
         o.write('\t'.join(gene[4:7])+'\t'+str(gene[3])+'\t'+str(gene[7])+'\n')
-    a[tax] = {x[4]:[x[5],x[7],x[3]] for x in a[tax]}
     o.close()
+    a[tax] = {x[4]: [x[3], x[7]] for x in a[tax]}
 
 # создаём словарь предыдущих в опреоне генов
 pio = {}
@@ -116,15 +131,15 @@ for line in kr:
 k.close()
 
 # выбираем из скоров в опероне только те, что лежат выше гена, минимальный записываем в файл
-for tax in a:
-    for gene in a[tax]:
-        mog = a[tax][gene][0]
-        if mog in mog_names:
-            s = []
-            for i in pio[gene]:
-                if i in a[tax]:
-                    s.append(a[tax][i][1:])
-            s.sort(key=lambda x: x[0])
+for tax in b:
+    for mog in b[tax]:
+        locus = b[tax][mog]
+        s = []
+        for i in pio[locus]:
+            if i in a[tax]:
+                s.append(a[tax][i])
+        if s:
+            s.sort(key=lambda x: x[1])
             o = open('rgenes/operonScores/'+mog, 'a')
             o.write(tax+'\t'+str(s[0][0])+'\t'+str(s[0][1])+'\n')
             o.close()
